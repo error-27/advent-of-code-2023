53f5dc417f952f9954ac65005ab14b2c54113b7d
[Error_27] <error27dev@gmail.com>
2023-12-01 21:13:49 -0500

    Second puzzle solve

--- src/day1.rs
+++ src/day1.rs
@@ -1,4 +1,4 @@
-pub fn solve(input: String) -> usize {
+pub fn solve(input: &String) -> usize {
     let lines = input.lines();
     let mut nums: Vec<usize> = Vec::new();
 
@@ -15,10 +15,79 @@ pub fn solve(input: String) -> usize {
             }
         }
         if digits[1] == ' ' {digits[1] = digits[0]}
+
         let d1: usize = digits[0].to_digit(10).unwrap() as usize;
         let d2: usize = digits[1].to_digit(10).unwrap() as usize;
 
         nums.push((d1*10) + d2);
+    }
+
+    nums.iter().map(|&i| i as usize).sum()
+}
+
+pub fn solve2(input: &String) -> usize {
+    let lines = input.lines();
+    let mut nums: Vec<usize> = Vec::new();
+    let numwords = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+    for line in lines {
+        let mut digit_indices: Vec<usize> = Vec::new();
+        let mut digits: [usize; 2] = [0, 0];
+        
+        let chars = line.chars();
+        for (pos, c) in chars.enumerate() {
+            if c.is_numeric() {
+                digit_indices.push(pos);
+            }
+        }
+
+        for word in numwords {
+            let i = line.find(word);
+            let j = line.rfind(word);
+
+            if i.is_none() || j.is_none() {
+                continue;
+            }
+
+            digit_indices.push(i.unwrap());
+            digit_indices.push(j.unwrap());
+        }
+        digit_indices.sort();
+
+        let first = digit_indices.first().unwrap();
+        let last = digit_indices.last().unwrap();
+
+        if line.chars().nth(*first).unwrap().is_numeric() {
+            digits[0] = line.chars().nth(*first).unwrap().to_digit(10).unwrap() as usize; // stupid long line lmao
+        }else{
+            for (pos, w) in numwords.iter().enumerate() {
+                let i = line.find(w);
+
+                if i.is_none() {continue;}
+
+                if i.unwrap() == *first {
+                    digits[0] = pos + 1;
+                    break;
+                }
+            }
+        }
+
+        if line.chars().nth(*last).unwrap().is_numeric() {
+            digits[1] = line.chars().nth(*last).unwrap().to_digit(10).unwrap() as usize; // stupid long line lmao
+        }else{
+            for (pos, w) in numwords.iter().enumerate() {
+                let i = line.rfind(w);
+
+                if i.is_none() {continue;}
+
+                if i.unwrap() == *last {
+                    digits[1] = pos + 1;
+                    break;
+                }
+            }
+        }
+
+        nums.push(10*digits[0] + digits[1]);
     }
 
     nums.iter().map(|&i| i as usize).sum()
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ mod day1;
 fn main() -> Result<()> {
     let content = include_str!("inputs/day1").to_string();
 
-    println!("{}", day1::solve(content));
+    println!("{}", day1::solve(&content));
+    println!("{}", day1::solve2(&content));
 
     Ok(())
 }
