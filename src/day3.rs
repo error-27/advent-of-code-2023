b5a45c95b731be03e99cd4cd48d9eb4094cb6723
[Error_27] <error27dev@gmail.com>
2023-12-03 17:21:21 -0500

    Day 3 puzzle 1 (running out of time for part 2)

---
+++ src/day3.rs
@@ -0,0 +1,91 @@
+use regex::{Regex, Match};
+
+const WIDTH: usize = 139;
+const HEIGHT: usize = 139;
+
+pub fn solve(input: &String) -> usize {
+    let mut solution: usize = 0;
+
+    let re = Regex::new(r"\d+").unwrap();
+    let mut numbers: Vec<Vec<Match>> = Vec::new();
+    let lines = input.lines();
+
+    for (y, line) in lines.enumerate() {
+        let results = re.find_iter(line);
+
+        for m in results {
+            let mut counted = false;
+            let x = m.start();
+            let x_end = m.end();
+
+            if x > 0 && line.chars().nth(x - 1).unwrap() != '.' { // left
+                counted = true;
+            }
+            if !counted && x_end - 1 < WIDTH && line.chars().nth(x_end).unwrap() != '.' { // right
+                counted = true;
+            }
+
+            if !counted && y > 0 { // symbols above
+                let above = input.lines().nth(y-1).unwrap();
+                for ix in x..x_end {
+                    if above.chars().nth(ix).unwrap() != '.' {
+                        counted = true;
+                        break;
+                    }
+                }
+            }
+
+            if !counted && y < HEIGHT { // symbols below
+                let below = input.lines().nth(y+1).unwrap();
+                for ix in x..x_end {
+                    if below.chars().nth(ix).unwrap() != '.' {
+                        counted = true;
+                        break;
+                    }
+                }
+            }
+
+            if !counted && y > 0 && x > 0 {
+                let above = input.lines().nth(y-1).unwrap();
+                println!("checking {}, {} for {}", x-1, y-1, m.as_str());
+                if above.chars().nth(x-1).unwrap() != '.' {
+                    counted = true;
+                }
+            }
+            if !counted && y > 0 && x_end - 1 < WIDTH {
+                let above = input.lines().nth(y-1).unwrap();
+                println!("checking {}, {} for {}", x_end, y-1, m.as_str());
+                if above.chars().nth(x_end).unwrap() != '.' {
+                    counted = true;
+                }
+            }
+            if !counted && y < HEIGHT && x > 0 {
+                let below = input.lines().nth(y+1).unwrap();
+                println!("checking {}, {} for {}", x-1, y+1, m.as_str());
+                if below.chars().nth(x-1).unwrap() != '.' {
+                    counted = true;
+                }
+            }
+            if !counted && y < HEIGHT && x_end - 1 < WIDTH {
+                let below = input.lines().nth(y+1).unwrap();
+                println!("checking {}, {} for {}", x_end, y+1, m.as_str());
+                if below.chars().nth(x_end).unwrap() != '.' {
+                    counted = true;
+                }
+            }
+
+            if counted {
+                println!("verified {}", m.as_str());
+                solution += m.as_str().parse::<usize>().unwrap();
+            }
+        }
+    }
+
+    solution
+}
+
+pub fn solve2(input: &String) -> usize {
+    let mut solution: usize = 0;
+
+    solution
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use std::io::Result;
 mod day1;
 mod day2;
+mod day3;
 
 fn main() -> Result<()> {
     let d1_file = include_str!("inputs/day1").to_string();
     let d2_file = include_str!("inputs/day2").to_string();
+    let d3_file = include_str!("inputs/day3").to_string();
 
-    println!("Day 1-1: {}", day1::solve(&d1_file));
-    println!("Day 1-2: {}", day1::solve2(&d1_file));
+    //println!("Day 1-1: {}", day1::solve(&d1_file));
+    //println!("Day 1-2: {}", day1::solve2(&d1_file));
 
-    println!("Day 2-1: {}", day2::solve(&d2_file));
-    println!("Day 2-2: {}", day2::solve2(&d2_file));
+    //println!("Day 2-1: {}", day2::solve(&d2_file));
+    //println!("Day 2-2: {}", day2::solve2(&d2_file));
+
+    println!("Day 3-1: {}", day3::solve(&d3_file));
 
     Ok(())
 }
