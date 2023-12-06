cfc339d3a5df459d17c7d4e8b9b6395bd3e4bd85
[Error_27] <error27dev@gmail.com>
2023-12-06 18:53:10 -0500

    Day 5 attempt 1: THIS DOES NOT WORK AND INFINITELY LOOPS

---
+++ src/day5.rs
@@ -0,0 +1,58 @@
+use regex::Regex;
+
+pub fn solve(input: &String) -> usize {
+    let mut solution: usize = 0;
+    let re_nums = Regex::new(r"\d+").unwrap();
+
+    let seeds: Vec<usize> = re_nums.find_iter(
+        input.lines().collect::<Vec<_>>().first().unwrap())
+        .map(|m| m.as_str().parse::<usize>().unwrap())
+        .collect();
+    
+    println!("{:?}", seeds);
+    
+    let re_maps = vec![
+        Regex::new(r"seed-to-soil map:\n((\d+ )(\d+ )(\d+)\n)+").unwrap(),
+        Regex::new(r"soil-to-fertilizer map:\n((\d+ )(\d+ )(\d+)\n)+").unwrap(),
+        Regex::new(r"fertilizer-to-water map:\n((\d+ )(\d+ )(\d+)\n)+").unwrap(),
+        Regex::new(r"water-to-light map:\n((\d+ )(\d+ )(\d+)\n)+").unwrap(),
+        Regex::new(r"light-to-temperature map:\n((\d+ )(\d+ )(\d+)\n)+").unwrap(),
+        Regex::new(r"temperature-to-humidity map:\n((\d+ )(\d+ )(\d+)\n)+").unwrap(),
+        Regex::new(r"humidity-to-location map:\n((\d+ )(\d+ )(\d+)\n)+").unwrap()
+    ];
+
+    let mut map_ranges: Vec<Vec<[usize; 3]>> = vec![];
+
+    for r in re_maps {
+        let text = r.find(input).unwrap().as_str();
+        let map_nums: Vec<usize> = re_nums.find_iter(text).map(|i| i.as_str().parse::<usize>().unwrap()).collect();
+        
+        let mut map: Vec<[usize; 3]> = vec![];
+        for i in 0..map_nums.len()/3 {
+            map.push([map_nums[i*3], map_nums[i*3+1], map_nums[i*3+2]]);
+        }
+
+        //println!("{:?}", map);
+        map_ranges.push(map);
+    }
+
+    solution = seeds[0];
+    for seed in seeds {
+        let mut value = seed;
+        for r in map_ranges.iter() {
+            let src_range: Vec<Vec<usize>> = r.iter().map(|i| (i[1]..i[1]+i[2]).collect()).collect();
+            let dest_range: Vec<Vec<usize>> = r.iter().map(|i| (i[0]..i[0]+i[2]).collect()).collect();
+
+            for i in 0..src_range.len() {
+                if !src_range[i].contains(&value) { continue; }
+                value = dest_range[i][src_range[i].binary_search(&value).unwrap()];
+                //println!("value stage: {}", value);
+            }
+        }
+
+        solution = solution.min(value);
+        //println!("value: {}", value);
+    }
+
+    solution
+}
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 fn main() -> Result<()> {
     //let d1_file = include_str!("inputs/day1").to_string();
     //let d2_file = include_str!("inputs/day2").to_string();
     //let d3_file = include_str!("inputs/day3").to_string();
-    let d4_file = include_str!("inputs/day4").to_string();
+    //let d4_file = include_str!("inputs/day4").to_string();
+    //let d5_file = include_str!("inputs/day5").to_string();
 
     //println!("Day 1-1: {}", day1::solve(&d1_file));
     //println!("Day 1-2: {}", day1::solve2(&d1_file));
@@ -18,8 +20,10 @@ fn main() -> Result<()> {
 
     //println!("Day 3-1: {}", day3::solve(&d3_file));
 
-    println!("Day 4-1: {}", day4::solve(&d4_file));
-    println!("Day 4-2: {}", day4::solve2(&d4_file));
+    //println!("Day 4-1: {}", day4::solve(&d4_file));
+    //println!("Day 4-2: {}", day4::solve2(&d4_file));
+
+    //println!("Day 5-1: {}", day5::solve(&d5_file));
 
     Ok(())
 }
